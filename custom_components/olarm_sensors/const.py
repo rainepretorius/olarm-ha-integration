ae6c7a8cd3afad5797553221d5e3fbd748a81ba6
Raine Pretorius <raine.pretorius1@gmail.com>
2023-04-26 11:43:47 +0200

    Updated version info

--- custom_components/olarm_sensors/const.py
+++ custom_components/olarm_sensors/const.py
@@ -11,7 +11,7 @@ from homeassistant.const import (
 from homeassistant.components.binary_sensor import BinarySensorDeviceClass
 
 
-VERSION = "2.0.6"
+VERSION = "2.0.7"
 
 DOMAIN = "olarm_sensors"
 AuthenticationError = "invalid_credentials"
