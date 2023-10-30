0254b69da5c277c91fe999c0645bda67006b264d
Cha0s2nd <cha0s2nd@outlook.com>
2023-10-30 12:07:26 +0200

    Update olarm_api.py
    Add checks for properties in pgm and ukeys

--- custom_components/olarm_sensors/olarm_api.py
+++ custom_components/olarm_sensors/olarm_api.py
@@ -377,10 +377,10 @@ class OlarmApi:
         return: (list): The pgm's for the alarm panel.
         """
         try:
-            pgm_state = devices_json["deviceState"]["pgm"]
-            pgm_labels = devices_json["deviceProfile"]["pgmLabels"]
-            pgm_limit = devices_json["deviceProfile"]["pgmLimit"]
-            pgm_setup = devices_json["deviceProfile"]["pgmControl"]
+            pgm_state = devices_json["deviceState"]["pgm"] if 'pgm' in devices_json["deviceState"] else []
+            pgm_labels = devices_json["deviceProfile"]["pgmLabels"] if 'pgmLabels' in devices_json["deviceProfile"] else []
+            pgm_limit = devices_json["deviceProfile"]["pgmLimit"] if 'pgmLimit' in devices_json["deviceProfile"] else []
+            pgm_setup = devices_json["deviceProfile"]["pgmControl"] if 'pgmControl' in devices_json["deviceProfile"] else []
 
         except (DictionaryKeyError, KeyError):
             # Error with PGM setup from Olarm app. Skipping PGM's
@@ -440,9 +440,18 @@ class OlarmApi:
 
         return: (list): The utility keys for the alarm panel.
         """
-        ukey_labels = devices_json["deviceProfile"]["ukeysLabels"]
-        ukey_limit = devices_json["deviceProfile"]["ukeysLimit"]
-        ukey_state = devices_json["deviceProfile"]["ukeysControl"]
+        try:
+            ukey_labels = devices_json["deviceProfile"]["ukeysLabels"] if 'ukeysLabels' in devices_json["deviceProfile"] else []
+            ukey_limit = devices_json["deviceProfile"]["ukeysLimit"] if 'ukeysLimit' in devices_json["deviceProfile"] else []
+            ukey_state = devices_json["deviceProfile"]["ukeysControl"] if 'ukeysControl' in devices_json["deviceProfile"] else []
+
+        except (DictionaryKeyError, KeyError):
+            # Error with Ukey setup from Olarm app. Skipping Ukey's
+            LOGGER.error(
+                "Error getting Ukey setup data for Olarm device (%s)", self.device_id
+            )
+            return []
+
         ukeys = []
         try:
             for i in range(0, ukey_limit):
